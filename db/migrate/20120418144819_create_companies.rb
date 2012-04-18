c9ebf54b67e6212e5709f616b7b8ef4535b233f3
weyewe <w.yunnal@gmail.com>
2012-04-18 21:50:51 +0700

    add company enrollment

---
+++ db/migrate/20120418144819_create_companies.rb
@@ -0,0 +1,21 @@
+class CreateCompanies < ActiveRecord::Migration
+  def change
+    create_table :companies do |t|
+      t.string :facebook_link
+      t.string :twitter_link
+      t.string :linkedin_link
+      
+      t.string :name 
+      t.string :phone_number
+      t.string :contact_email 
+      t.string :website_address
+      
+      t.text_area :office_address
+      
+      t.integer  :delivery_method,       :integer,     :default => NOTIFICATION_DELIVERY_METHOD[:instant]
+      t.string   :scheduled_delivery_hours, :string  , :default => ""
+
+      t.timestamps
+    end
+  end
+end
